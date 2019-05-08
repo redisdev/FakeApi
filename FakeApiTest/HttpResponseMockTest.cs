32db2a5954f42d7c63e31cf7719dca136f867ffc
peephole8062 <redisdev80@gmail.com>
2019-05-08 23:00:11 +0200

    Unit test

--- FakeApiTest/HttpResponseMockTest.cs
+++ FakeApiTest/HttpResponseMockTest.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using FakeApi;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -24,6 +26,34 @@ namespace FakeApiTest
             Assert.AreEqual("2", response.GetNextFile());
             Assert.AreEqual("3", response.GetNextFile());
             Assert.AreEqual("1", response.GetNextFile());
+        }
+
+        [TestMethod]
+        public void GetNextFile_ShouldThrowInvalidExceptionWhenFilesIsNull()
+        {
+            //Arrange
+            var response = new HttpResponseMock();
+            response.Files = null;
+
+            //Act
+            var ex = Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                response.GetNextFile();
+            });
+        }
+
+        [TestMethod]
+        public void GetNextFile_ShouldThrowInvalidExceptionWhenFilesIsEmpty()
+        {
+            //Arrange
+            var response = new HttpResponseMock();
+            response.Files = new List<string>();
+
+            //Act
+            var ex = Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                response.GetNextFile();
+            });
         }
     }
 }
